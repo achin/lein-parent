7f25d4b4539763faaeddad17279b67a8bb9493b6
Alex Chin <alex.ba.chin@gmail.com>
2017-08-20 20:45:25 -0400

    Prepare for next release.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-parent "0.3.2"
+(defproject lein-parent "0.3.3-SNAPSHOT"
   :description "Leiningen plugin for inheriting properties from a parent project"
   :url "https://github.com/achin/lein-parent"
   :license {:name "Eclipse Public License"
