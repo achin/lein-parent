23144849622ef4255c2865a0dc240e8c6fba9d71
Frozenlock <frozenlock@gmail.com>
2020-06-21 16:52:05 -0400

    Add child test template for 'inherit all'

---
+++ dev-resources/t_parent/samples/children/with_parent_inherit_all/project.clj
@@ -0,0 +1,4 @@
+(defproject child-with-parent-with-profiles "0.0.1"
+  :description "Child project that references parent project with properties"
+  :parent-project {:path    "../../parents/with_properties/project.clj"
+                   :inherit [:leiningen.parent/all]})
